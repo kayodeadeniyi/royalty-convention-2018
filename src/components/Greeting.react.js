71fa6bc024cc0576a137d182af393e2935adb1e7
Abdulsemiu Atanda <semiu@live.com>
2018-12-03 14:32:49 +0100

    style greeting component

--- src/components/Greeting.react.js
+++ src/components/Greeting.react.js
@@ -1,14 +1,28 @@
 import React from "react"
-import {View, Text} from "react-native"
+import {View, Text, StyleSheet} from "react-native"
 
 class Greeting extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Hello My dude!</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello My dude!</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
 
 export default Greeting
